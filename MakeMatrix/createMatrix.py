8947e19d06ee4a7fece457c96aeeaa5d641f0114
Giri Gaurav Bhatnagar <giri1gb@gmail.com>
2017-07-18 10:15:28 +0530

    Write matrix one sample at a time

--- MakeMatrix/createMatrix.py
+++ MakeMatrix/createMatrix.py
@@ -1,10 +1,10 @@
-# NOTE: Appending 'FAIL.' with filename
+    # NOTE: Appending 'FAIL.' with filename
 # TODO: Make file reading with chunk
-import glob, sys
+import glob, sys, os 
 import pandas as pd
 from util import addColumn
 
-OUTPUT_DIR = '/Users/Apple/workspace/ucsc/output'
+OUTPUT_DIR = '/Users/Apple/workspace/ucsc/output' #'/home/ec2-user/efs/code/outputs'
 OUTPUT_MATRIX_FOLDER = './output'
 
 cfg = open('makematrix.cfg','r')
@@ -15,55 +15,66 @@ cfg.close()
 samples = pd.read_csv('../test_samples.txt',sep='\t')
 
 for param_config in cfg_data:
-	'''
-	param - which parameter to extract
-	param_file - from which file to extract
-	'''
-	# strip extra whitespaces
-	param,param_file = map(str.strip,param_config.split('\t'))
-	
-	print(param,param_file)
-	
-	df = pd.DataFrame()	
+    '''
+    param - which parameter to extract
+    param_file - from which file to extract
+    '''
+    # strip extra whitespaces
+    param,param_file = map(str.strip,param_config.split('\t'))
+    
+    print(param,param_file)
+    
+    df = pd.DataFrame()    
+    count = 0
+    # For each sample find the param file and extract the appropriate column 
+    for row in samples.values:
+        #print(row)
+        sample_filename,sample_id,sample_gnosid = row
 
-	# For each sample find the param file and extract the appropriate column 
-	for row in samples.values:
-		sample_filename,sample_id,sample_gnosid = row
+        '''
+        sample_param_file - Absolute path to the param file
+        '''
+        # sample_filename.split('.')[0] gets the 'filename' from 'filename.tar.gz'
+        sample_param_file = '{}/{}/{}/{}'.format(OUTPUT_DIR,sample_gnosid,'FAIL.'+sample_filename.split('.')[0],param_file )
+        
+        # If file exists 
+        if glob.glob(sample_param_file) != []:
+            
+            # Read the file 
+            data = pd.read_csv(sample_param_file,sep='\t')
+    
+            try:
+                # Add 'id' column in df if not already present
+                if not 'id' in df:    
+                    # Extract the very first column and set that as 'id' column for the  output matrix
+                    df['id'] = data[data.columns.values[0]].values
+                    col = df['id']
+                    # print(df['id'])
+                    # Delete an already existing file.
+                    os.system('rm {}/{}_mat.tsv'.format(OUTPUT_MATRIX_FOLDER,param))
+                    # col is a Pandas series object
+                    # col.set_value(0,id)
+                    addColumn('{}/{}_mat.tsv'.format(OUTPUT_MATRIX_FOLDER,param),col,header='id',sep='\t')
 
-		'''
-		sample_param_file - Absolute path to the param file
-		'''
-		# sample_filename.split('.')[0] gets the 'filename' from 'filename.tar.gz'
-		sample_param_file = '{}/{}/{}/{}'.format(OUTPUT_DIR,sample_gnosid,'FAIL.'+sample_filename.split('.')[0],param_file )
-		
-		# If file exists 
-		if glob.glob(sample_param_file) != []:
-			
-			# Read the file 
-			data = pd.read_csv(sample_param_file,sep='\t')
-	
-			try:
-				# Add 'id' column in df if not already present
-				if not 'id' in df:	
-					# Extract the very first column and set that as 'id' column for the  output matrix
-					df['id'] = data[data.columns.values[0]].values
-					# print(df['id'])
-					df.set_index('id')
+                    # df.set_index('id')
 
-				#Extract param column 
-				col = data[param]
-				# Add column to data_frame, under header 'gnosId'
-				df[sample_gnosid] = col.values
-					
-				print(sys.getsizeof(df))
-			except Exception as e:
-				print("ERROR: {} does not contain {} header.".format(sample_param_file,param))
-				print(e)
-		else:
-			print('ERROR: File does not exist :',sample_param_file )
+                #Extract param column 
+                col = data[param]
+                # Add column to data_frame, under header 'gnosId'
+                # TODO: Write the column here. 
+                # col is a Pandas series object
+                # col.set_value(0,sample_gnosid)
+                addColumn('{}/{}_mat.tsv'.format(OUTPUT_MATRIX_FOLDER,param),col,header=sample_gnosid, sep='\t')
 
-	# Write df as a tab-separated file
-	# index=False - do not write the default index with the file
-	df.to_csv('{}/{}_mat.tsv'.format(OUTPUT_MATRIX_FOLDER,param),sep='\t',index=False)
+                # df[sample_gnosid] = col.values
+                    
+               # print(sys.getsizeof(df))
+            except Exception as e:
+                print("ERROR: {} does not contain {} header.".format(sample_param_file,param))
+                print(e)
+        else:
+            print('ERROR: File does not exist :',sample_param_file )
 
-
+    # Write df as a tab-separated file
+    # index=False - do not write the default index with the file
+    # df.to_csv('{}/{}_mat.tsv'.format(OUTPUT_MATRIX_FOLDER,param),sep='\t',index=False)
