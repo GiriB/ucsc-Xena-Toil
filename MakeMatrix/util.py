26b22d59b953097b52f4f29f2c4fea750e4cb28d
Giri Gaurav Bhatnagar <giri1gb@gmail.com>
2017-07-18 10:30:37 +0530

    Util.py for MakeMatrix

---
+++ MakeMatrix/util.py
@@ -0,0 +1,48 @@
+import os, glob
+
+#TODO: 
+
+'''
+Add a column to end of a file. If the file does not exist, it is created. 
+fileName - Name of the file 
+columnList - list of values to be inserted
+sep - column separator; default - tab 
+EOL - End Of Line; default - newline 
+'''
+
+def addColumn(fileName, columnList, header='UNK',sep='\t', EOL='\n'):
+    #The number of lines in the file should be same as the number of the columns 
+    # print("Adding column to ",columnList[:10])
+    columnList = [header] + list(columnList)
+    if glob.glob(fileName) == []:
+        # The file does not exist yet. So create an empty file and insert the first column.
+        try:
+            # print("THERE WAS AN EMPTY FILE.")
+            with open(fileName,'w') as fwrite :                
+                for count in range(len(columnList)):
+                    fwrite.write('{}{}'.format(columnList[count],EOL))
+        except Exception as e:
+            print(e)
+    else:
+        try:
+            # Copy the file to tmp file and insert the extra column. 
+            # Later rename the tmp file to the original.
+            with open(fileName,'r') as fread :
+                with open(fileName+'.tmp','w') as fwrite:                
+                    for count,line in enumerate(fread):
+                        fwrite.write(line.strip())
+                        # Insert a separator before inserting a new column.
+                        fwrite.write(sep)
+                        fwrite.write('{}{}'.format(columnList[count],EOL))
+            
+            # Rename the tmp file.                          
+            os.system('mv {} {}'.format(fileName+'.tmp',fileName))
+            os.system('rm -rf {}'.format(fileName+'.tmp'))
+        except Exception as e:
+            print(e)
+'''
+'''
+def getMapping(fileID):
+    #Given fileID return Submitter Specimien ID from mappings.tsv
+    for i in open('mappings.tsv')
+
