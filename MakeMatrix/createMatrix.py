00d48e7d07c0846c56fc9519acf79020fd44afc8
Giri Gaurav Bhatnagar <giri1gb@gmail.com>
2017-07-20 13:02:09 +0530

    Adds Writing to file in chunks

--- MakeMatrix/createMatrix.py
+++ MakeMatrix/createMatrix.py
@@ -2,10 +2,11 @@
 # TODO: Make file reading with chunk
 import glob, sys, os 
 import pandas as pd
-from util import addColumn
+from util import addColumns, getMapping
 
 OUTPUT_DIR = '/Users/Apple/workspace/ucsc/output' #'/home/ec2-user/efs/code/outputs'
 OUTPUT_MATRIX_FOLDER = './output'
+CHUNK_SIZE = 1
 
 cfg = open('makematrix.cfg','r')
 cfg_data = cfg.readlines()
@@ -25,9 +26,14 @@ for param_config in cfg_data:
     print(param,param_file)
     
     df = pd.DataFrame()    
+   
     count = 0
+    cols_chunk = []
+    headers_chunk = []
+   
     # For each sample find the param file and extract the appropriate column 
-    for row in samples.values:
+    for row_ind, row in enumerate(samples.values):
+        print("GIRI")
         #print(row)
         sample_filename,sample_id,sample_gnosid = row
 
@@ -44,29 +50,38 @@ for param_config in cfg_data:
             data = pd.read_csv(sample_param_file,sep='\t')
     
             try:
+                # pass
                 # Add 'id' column in df if not already present
                 if not 'id' in df:    
                     # Extract the very first column and set that as 'id' column for the  output matrix
-                    df['id'] = data[data.columns.values[0]].values
-                    col = df['id']
-                    # print(df['id'])
+                    df['id']  = data[data.columns.values[0]].values
+                    
+                    
                     # Delete an already existing file.
+                    os.system('touch {}/{}_mat.tsv'.format(OUTPUT_MATRIX_FOLDER,param))
                     os.system('rm {}/{}_mat.tsv'.format(OUTPUT_MATRIX_FOLDER,param))
                     # col is a Pandas series object
-                    # col.set_value(0,id)
-                    addColumn('{}/{}_mat.tsv'.format(OUTPUT_MATRIX_FOLDER,param),col,header='id',sep='\t')
-
-                    # df.set_index('id')
+                
+                    #Add ID column first
+                    addColumns('{}/{}_mat.tsv'.format(OUTPUT_MATRIX_FOLDER,param),[df['id']],['id'],sep='\t')
 
                 #Extract param column 
-                col = data[param]
+                cols_chunk.append(data[param])
+                headers_chunk.append(getMapping(sample_id))
+
                 # Add column to data_frame, under header 'gnosId'
                 # TODO: Write the column here. 
                 # col is a Pandas series object
                 # col.set_value(0,sample_gnosid)
-                addColumn('{}/{}_mat.tsv'.format(OUTPUT_MATRIX_FOLDER,param),col,header=sample_gnosid, sep='\t')
-
-                # df[sample_gnosid] = col.values
+                # If we have reac CHUNK or we have reached the last index 
+                print(type(samples.values))
+                if count == CHUNK_SIZE or row_ind == len(samples.values)-1:
+                    addColumns('{}/{}_mat.tsv'.format(OUTPUT_MATRIX_FOLDER,param),cols_chunk,headers_chunk, sep='\t')
+                    count = 0
+                    cols_chunk = []
+                    headers_chunk = []
+                else:
+                    count+=1
                     
                # print(sys.getsizeof(df))
             except Exception as e:
@@ -74,7 +89,3 @@ for param_config in cfg_data:
                 print(e)
         else:
             print('ERROR: File does not exist :',sample_param_file )
-
-    # Write df as a tab-separated file
-    # index=False - do not write the default index with the file
-    # df.to_csv('{}/{}_mat.tsv'.format(OUTPUT_MATRIX_FOLDER,param),sep='\t',index=False)
--- MakeMatrix/util.py
+++ MakeMatrix/util.py
@@ -41,11 +41,55 @@ def addColumn(fileName, columnList, header='UNK',sep='\t', EOL='\n'):
         except Exception as e:
             print(e)
 
+def addColumns(fileName, columnsList, headersList,sep='\t', EOL='\n'):
+    # The number of headers and the columns shouldbe equal 
+    print("INSIDE")
+    assert len(headersList) == len(columnsList)
+
+    columnsList = [[headersList[count]] + list(columnsList[count]) for count in range(len(columnsList))]
+    print(columnsList[0][:5])
+    # print("*****************************")
+    #TODO: No need to keep headers and columns separately
+
+    if glob.glob(fileName) == []:
+        # The file does not exist yet. So create an empty file and insert the first column.
+        try:
+            # print("THERE WAS AN EMPTY FILE.")
+            with open(fileName,'w') as fwrite :
+                # For count uptp length of each column (each column length should be equal)
+                for count in range(len(columnsList[0])):
+                    # print("------------",sep.join([columnList[count] for columnList in columnsList])[:10])
+                    fwrite.write('{}{}'.format(sep.join([columnList[count] for columnList in columnsList]),EOL))                
+        except Exception as e:
+            print("LAMBA",e)
+    else:
+        try:
+            # Copy the file to tmp file and insert the extra column(s). 
+            # Later rename the tmp file to the original.
+            with open(fileName,'r') as fread :
+                with open(fileName+'.tmp','w') as fwrite:                
+
+                    for count,line in enumerate(fread):
+                        fwrite.write(line.strip())
+                        # Insert a separator before inserting a new column.
+                        fwrite.write(sep)
+                        # print("HELLO",columnsList)
+                        
+                        # print()
+                        # print("---------------------------------------------------------")
+                        fwrite.write('{}{}'.format(sep.join(list(map(str,[columnList[count] for columnList in columnsList]))),EOL))                
+            
+            # Rename the tmp file.                          
+            os.system('mv {} {}'.format(fileName+'.tmp',fileName))
+            os.system('rm -rf {}'.format(fileName+'.tmp'))
+        except Exception as e:
+            print("SIMBA",e)
+
 
 fileIdToSpecimenIdMapping = {}
 with open('mappings.tsv') as f:
     #Ignore first line
-    f.next()
+    next(f)
     for line in f:
         fileId,SpecimenId,gnosId = line.strip().split()
         fileIdToSpecimenIdMapping[fileId] = SpecimenId
@@ -57,6 +101,8 @@ Returns SpecimenId for a given FileId
 def getMapping(fileId):
     #Given fileId return Submitter Specimien ID from mappings.tsv
     try:
+        # return "XXXX"
         return fileIdToSpecimenIdMapping[fileId]
     except Exception as e:
-        print("ERROR: ",e)
+        print("ERROR:XXXX ",e)
+        return "XXXX"
